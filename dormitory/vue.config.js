6e9dac674770d79844f8966da51ab32bb3734ef5
KAMUZUKI <1437487442@qq.com>
2022-12-13 14:32:55 +0800

    uploads

--- dormitory/vue.config.js
+++
@@ -1,6 +0,0 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true,
-  publicPath:'/dormitory-ui/',
-  outputDir:'dormitory-ui',
-})
